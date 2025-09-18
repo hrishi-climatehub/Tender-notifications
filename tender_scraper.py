e94289ba8f0b89c2c3be1802c56a89d2e43581db
hrishi-climatehub <hrishi@climatehub.in>
2025-09-18 17:45:40 +0530

    Update tender_scraper.py

--- tender_scraper.py
+++ tender_scraper.py
@@ -13,7 +13,7 @@ creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
 client = gspread.authorize(creds)
 
 # Open the Google Sheet by URL or name
-sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1PtyjgDfe7VYmtpW-UlZM0U84Y6iE9GwR3W3FSSsvj0g/edit?usp=sharing").sheet1  # or .worksheet("Sheet1")
+sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1PtyjgDfe7VYmtpW-UlZM0U84Y6iE9GwR3W3FSSsvj0g/edit?usp=sharing").Tenders  # or .worksheet("Sheet1")
 
 # Example: append a row of tender data
 sheet.append_row(["Date", "Tender Title", "Link", "Budget"])
